72464cd43ec74efc84a39efc5fd83803d98d52c8
@laura.lululand <laura.lululand@gmail.com>
2020-09-18 09:35:34 +0100

    Stop drag into each other!

--- js/adapt-walkgrove-dragdrop.js
+++ js/adapt-walkgrove-dragdrop.js
@@ -20,13 +20,6 @@ define([
 
     postRender: function() {
       this.setReadyStatus();
-
-      // let count = 0;
-      // for( var intItem in this.model.get( '_items' ) ) {
-      //   console.log("dragdrop__item-" + count);
-      //   this.dragElement(document.getElementById("dragdrop__item-" + count));
-      //   count++;
-      // }
     },
 
     checkIfResetOnRevisit: function() {
@@ -43,57 +36,16 @@ define([
     },
     
     drag: function(ev) {
-      console.log("drag: " + ev.target.id);
       _draggable = ev.target.id;
     },
     
     drop: function(ev) {
       ev.preventDefault();
-      // var data = ev.dataTransfer.getData("text");
-      ev.target.appendChild(document.getElementById(_draggable));
+      if (ev.target.classList.contains('js-drag-target')) {
+        console.log(ev.target);
+        ev.target.appendChild(document.getElementById(_draggable));
+      }
     },
-
-    // dragElement: function(elmnt) {
-    //   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    //   if (document.getElementById(elmnt.id)) {
-    //     // if present, the header is where you move the DIV from:
-    //     document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-    //   } else {
-    //     // otherwise, move the DIV from anywhere inside the DIV:
-    //     elmnt.onmousedown = dragMouseDown;
-    //   }
-
-    //   function dragMouseDown(e){
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     // get the mouse cursor position at startup:
-    //     pos3 = e.clientX;
-    //     pos4 = e.clientY;
-    //     document.onmouseup = closeDragElement;
-    //     // call a function whenever the cursor moves:
-    //     document.onmousemove = elementDrag;
-    //   }
-
-    //   function elementDrag(e) {
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     // calculate the new cursor position:
-    //     pos1 = pos3 - e.clientX;
-    //     pos2 = pos4 - e.clientY;
-    //     pos3 = e.clientX;
-    //     pos4 = e.clientY;
-    //     // set the element's new position:
-    //     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    //     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    //   }
-
-    //   function closeDragElement() {
-    //     // stop moving when mouse button is released:
-    //     document.onmouseup = null;
-    //     document.onmousemove = null;
-    //     alert("released");
-    //   }
-    // },
 
   },
   {
